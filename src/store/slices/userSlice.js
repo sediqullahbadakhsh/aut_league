cf4300d8a6e1fa7aa9957148969b9ed1845f81c4
sediqbadakhsh <sediq@invasso.com>
2023-03-10 17:44:45 +0430

    create user slice and thunk

---
+++ src/store/slices/userSlice.js
@@ -0,0 +1,62 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { login, addUser, updateUser, logout } from "../thunks/userThunk";
+
+const userSlice = createSlice({
+  name: "users",
+  initialState: {
+    data: [],
+    isLoading: false,
+    error: null,
+  },
+  extraReducers(builder) {
+    builder.addCase(login.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(login.fulfilled, (state, action) => {
+      state.data = action.payload;
+      state.isLoading = false;
+    });
+    builder.addCase(login.rejected, (state, action) => {
+      state.isLoading = false;
+      state.error = action.error;
+    });
+    builder.addCase(addUser.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(addUser.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.data.push(action.payload);
+    });
+    builder.addCase(addUser.rejected, (state, action) => {
+      state.isLoading = false;
+      state.error = action.error;
+    });
+    builder.addCase(updateUser.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(updateUser.fulfilled, (state, action) => {
+      state.isLoading = false;
+      const updatedUserIndex = state.data.findIndex(
+        (user) => user.id === action.payload.id
+      );
+      state.data[updatedUserIndex] = action.payload;
+    });
+    builder.addCase(updateUser.rejected, (state, action) => {
+      state.isLoading = false;
+      state.error = action.error;
+    });
+    builder.addCase(logout.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(logout.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.data = state.data.filter((user) => user.id !== action.payload.id);
+    });
+    builder.addCase(logout.rejected, (state, action) => {
+      state.isLoading = false;
+      state.error = action.error;
+    });
+  },
+});
+
+export const userReducer = userSlice.reducer;
---
+++ src/store/thunks/userThunk.js
@@ -0,0 +1,31 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { BASE_URL } from "../baseURL";
+import axios from "axios";
+
+const login = createAsyncThunk("users/fetch", async (user) => {
+  const response = await axios.post(`${BASE_URL}user/login`, {
+    ...user,
+  });
+  localStorage.setItem("token", response.data["access-token"]);
+  return response.data;
+});
+
+const addUser = createAsyncThunk("users/add", async (user) => {
+  const response = await axios.post(`${BASE_URL}user/register`, {
+    ...user,
+  });
+  return response.data;
+});
+
+const updateUser = createAsyncThunk("users/update", async (user) => {
+  const response = await axios.put(BASE_URL + user.id, {
+    ...user,
+  });
+  return response.data;
+});
+
+const logout = createAsyncThunk("users/logout", async () => {
+  localStorage.removeItem("token");
+});
+
+export { login, addUser, updateUser, logout };
