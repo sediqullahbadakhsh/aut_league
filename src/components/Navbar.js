bb9f79dc70b0a50dd791a6375d1546dd1ae847eb
sediqbadakhsh <sediq@invasso.com>
2023-03-10 17:45:54 +0430

    create navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,33 +2,43 @@ import { Link } from "react-router-dom";
 import "../assets/navbar.css";
 function Navbar() {
   return (
-    <nav dir="RTL">
-      <ul className="navbar">
-        <li className="navbar-item">
-          <Link to="/">صفحه اصلی</Link>
-        </li>
-        <li className="navbar-item">
-          <a href="#"></a>
-        </li>
-        <li className="navbar-item">
-          <a href="#">لیگ ها</a>
-          <ul className="navbar-dropdown">
-            <li className="navbar-dropdown-item">
-              <Link to="/simulation">شبه سازی</Link>
-            </li>
-            <li className="navbar-dropdown-item">
-              <a href="#">فزیکی</a>
-            </li>
-          </ul>
-        </li>
-        <li className="navbar-item">
-          <Link to="/dashboard">دشبورد</Link>
-        </li>
-        <li className="navbar-item">
-          <Link to="/contact">تماس باما</Link>
-        </li>
-      </ul>
-    </nav>
+    <div className="header">
+      <div>
+        <p>Logo</p>
+      </div>
+      <nav dir="RTL">
+        <ul className="navbar">
+          <li className="navbar-item">
+            <Link to="/">صفحه اصلی</Link>
+          </li>
+          <li className="navbar-item">
+            <a href="#"></a>
+          </li>
+          <li className="navbar-item">
+            <a href="#">لیگ ها</a>
+            <ul className="navbar-dropdown">
+              <li className="navbar-dropdown-item">
+                <Link to="/simulation">شبه سازی</Link>
+              </li>
+              <li className="navbar-dropdown-item">
+                <a href="#" disabled>
+                  فزیکی
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li className="navbar-item">
+            <Link to="/dashboard">دشبورد</Link>
+          </li>
+          <li className="navbar-item">
+            <Link to="/contact">تماس باما</Link>
+          </li>
+        </ul>
+      </nav>
+      <div>
+        <Link to="/signin">ثبت نام/ورود</Link>
+      </div>
+    </div>
   );
 }
 
