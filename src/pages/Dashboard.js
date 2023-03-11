8d76cbfc580e6731b2813f54bc068fe16f0e1ae7
sediqbadakhsh <sediq@invasso.com>
2023-03-12 01:11:27 +0430

    create dashboard routes

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,3 +1,31 @@
+import { useState } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+import DashboardContent from "../components/DashboardContent";
+import Teams from "../components/Teams";
+import Members from "../components/Members";
+import Sidbar from "../components/Sidbar";
+
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  const [location, setLocation] = useState("/dashboard");
+  const { pathname } = useLocation();
+
+  if (location !== pathname) {
+    setLocation(pathname);
+  }
+  return (
+    <div dir="rtl" className=" dashboard">
+      <Sidbar />
+      <div className="dashboard-content">
+        {location === "/dashboard" ? (
+          <DashboardContent />
+        ) : (
+          <Routes>
+            <Route path="/dashboard" element={<DashboardContent />} />
+            <Route path="league/teams" element={<Teams />} />
+            <Route path="league/members" element={<Members />} />
+          </Routes>
+        )}
+      </div>
+    </div>
+  );
 }
