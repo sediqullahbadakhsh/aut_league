a859ea5f38b189ac7bbd5c99788991bec5700bf1
sediqbadakhsh <sediq@invasso.com>
2023-03-12 01:14:36 +0430

    update side bar

--- src/components/Sidbar.js
+++ src/components/Sidbar.js
@@ -4,9 +4,6 @@ export default function Sidbar({ setComponent }) {
   return (
     <div className="sidebar">
       <ul>
-        {/* <li onClick={() => setComponent("dashboard")}>داشبورد</li>
-        <li onClick={() => setComponent("members")}>اعضا</li>
-        <li onClick={() => setComponent("teams")}>تیم ها</li> */}
         <li>
           <Link style={{ color: "black" }} to="/dashboard">
             داشبورد
