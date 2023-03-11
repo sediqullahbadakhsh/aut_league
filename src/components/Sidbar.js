eb9dc5d3230bee70aa1f84f48f0699d70e9b3952
sediqbadakhsh <sediq@invasso.com>
2023-03-12 01:11:59 +0430

    create sidebar

--- src/components/Sidbar.js
+++ src/components/Sidbar.js
@@ -1,10 +1,27 @@
+import { Link } from "react-router-dom";
+
 export default function Sidbar({ setComponent }) {
   return (
     <div className="sidebar">
       <ul>
-        <li onClick={() => setComponent("dashboard")}>داشبورد</li>
+        {/* <li onClick={() => setComponent("dashboard")}>داشبورد</li>
         <li onClick={() => setComponent("members")}>اعضا</li>
-        <li onClick={() => setComponent("teams")}>تیم ها</li>
+        <li onClick={() => setComponent("teams")}>تیم ها</li> */}
+        <li>
+          <Link style={{ color: "black" }} to="/dashboard">
+            داشبورد
+          </Link>
+        </li>
+        <li>
+          <Link style={{ color: "black" }} to="/dashboard/league/members">
+            اعضا
+          </Link>
+        </li>
+        <li>
+          <Link style={{ color: "black" }} to="/dashboard/league/teams">
+            تیم ها
+          </Link>
+        </li>
       </ul>
     </div>
   );
