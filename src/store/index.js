e82ed770774e778a3bed089bcc31fcb4d75d3581
sediqbadakhsh <sediq@invasso.com>
2023-03-12 02:23:11 +0430

    update store with members reducer

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { userReducer } from "./slices/userSlice";
 import { teamReducer } from "./slices/teamSlice";
+import { memberReducer } from "./slices/memberSlice";
+
 export const store = configureStore({
   reducer: {
     users: userReducer,
     teams: teamReducer,
+    members: memberReducer,
   },
 });
 
 export * from "./thunks/userThunk";
+export * from "./thunks/teamThunk";
+export * from "./thunks/memberThunk";
