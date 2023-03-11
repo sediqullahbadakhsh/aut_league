22d63232ad9a81002e239a115184ba80da6927c3
sediqbadakhsh <sediq@invasso.com>
2023-03-12 01:14:05 +0430

    update store

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { userReducer } from "./slices/userSlice";
+import { teamReducer } from "./slices/teamSlice";
 export const store = configureStore({
   reducer: {
     users: userReducer,
+    teams: teamReducer,
   },
 });
 
