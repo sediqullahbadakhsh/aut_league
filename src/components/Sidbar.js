19aa0e3464c108ea66f8efd58dba0988c0024371
sediqbadakhsh <sediq@invasso.com>
2023-03-13 04:05:45 +0430

    fix sidebar

--- src/components/Sidbar.js
+++ src/components/Sidbar.js
@@ -1,8 +1,26 @@
-import { Link } from "react-router-dom";
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { logout } from "../store/thunks/userThunk";
 
-export default function Sidbar({ setComponent }) {
+export default function Sidbar() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  // logout page
+  const handleLogout = (e) => {
+    dispatch(logout());
+    navigate("/");
+    window.location.reload();
+  };
+
   return (
     <div className="sidebar">
+      <div>
+        <p style={{ cursor: "pointer" }} onClick={handleLogout}>
+          خروج
+        </p>
+      </div>
       <ul>
         <li>
           <Link style={{ color: "black" }} to="/dashboard">
