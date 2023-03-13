bf32f7655e73f78933025c4e97cb69e85826dd0b
sediqbadakhsh <sediq@invasso.com>
2023-03-13 06:00:22 +0430

    add teammember reducer

--- src/store/index.js
+++ src/store/index.js
@@ -2,15 +2,18 @@ import { configureStore } from "@reduxjs/toolkit";
 import { userReducer } from "./slices/userSlice";
 import { teamReducer } from "./slices/teamSlice";
 import { memberReducer } from "./slices/memberSlice";
+import { teamMemberReducer } from "./slices/teamMemberSlice";
 
 export const store = configureStore({
   reducer: {
     users: userReducer,
     teams: teamReducer,
     members: memberReducer,
+    teamMembers: teamMemberReducer,
   },
 });
 
 export * from "./thunks/userThunk";
 export * from "./thunks/teamThunk";
 export * from "./thunks/memberThunk";
+export * from "./thunks/teamMemberThunk";
