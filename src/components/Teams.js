18806c6fec20c73836222e9b13d504b535a0b64a
sediqbadakhsh <sediq@invasso.com>
2023-03-16 03:25:12 +0430

    create pagination

--- src/components/Teams.js
+++ src/components/Teams.js
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { fetchTeam, removeTeam } from "../store/thunks/teamThunk";
 import TeamsModal from "./TeamsModal";
 import ViewTeam from "./ViewTeam";
+import ReactPaginate from "react-paginate";
 
 function Teams() {
   const teams = useSelector((state) => state.teams.data.teams);
@@ -12,17 +13,32 @@ function Teams() {
   const [editData, setEditData] = useState();
   const [viewData, setViewData] = useState();
   const dispatch = useDispatch();
+  const [currentPage, setCurrentPage] = useState(0);
+  const itemsPerPage = 10;
+
   useEffect(() => {
     dispatch(fetchTeam());
-  }, [dispatch]);
-  const hundelRemove = (id) => {
+  }, [dispatch, fetchTeam]);
+
+  const handleRemove = (id) => {
     dispatch(removeTeam({ id }));
   };
 
+  const handlePageClick = (data) => {
+    setCurrentPage(data.selected);
+  };
+
+  const teamsToDisplay = teams?.slice(
+    currentPage * itemsPerPage,
+    (currentPage + 1) * itemsPerPage
+  );
+
   return (
-    <div dir="rtl">
-      <h1>تیم ها</h1>
-      <button onClick={() => setShow(true)}>ایجاد تیم</button>
+    <div className="list-main" dir="rtl">
+      <h1 className="header_text">تیم ها</h1>
+      <button className="add-btn" onClick={() => setShow(true)}>
+        جدید
+      </button>
       {show && <TeamsModal setShow={setShow} />}
       <div>
         <div className="heading"></div>
@@ -39,42 +55,61 @@ function Teams() {
           </tr>
         </thead>
         <tbody>
-          {teams &&
-            teams.map((team) => (
-              <tr key={team.id}>
-                <td>{team.name}</td>
-                {team.category === 1 ? <td>شبه سازی</td> : <td>فزیکی</td>}
-                <td>{team.phone}</td>
-                <td>{team.address}</td>
-                <td style={{ display: "flex", gap: "5px", cursor: "pointer" }}>
-                  <p
-                    onClick={() => {
-                      setViewTeam(true);
-                      setViewData(team);
-                    }}
-                  >
-                    view
-                  </p>
-                  {viewTeam && (
-                    <ViewTeam viewData={viewData} setViewTeam={setViewTeam} />
-                  )}
-                  <p
-                    onClick={() => {
-                      setShowEdit(true);
-                      setEditData(team);
-                    }}
-                  >
-                    edit
-                  </p>
-                  {showEdit && (
-                    <TeamsModal setShowEdit={setShowEdit} editData={editData} />
-                  )}
-                  <p onClick={() => hundelRemove(team.id)}>remove</p>
-                </td>
-              </tr>
-            ))}
+          {teamsToDisplay?.map((team) => (
+            <tr key={team.id}>
+              <td>{team.name}</td>
+              {team.category === 1 ? <td>شبه سازی</td> : <td>فزیکی</td>}
+              <td>{team.phone}</td>
+              <td>{team.address}</td>
+              <td className="actions">
+                <p
+                  onClick={() => {
+                    setViewTeam(true);
+                    setViewData(team);
+                  }}
+                >
+                  view
+                </p>
+                {viewTeam && (
+                  <ViewTeam viewData={viewData} setViewTeam={setViewTeam} />
+                )}
+                <p
+                  onClick={() => {
+                    setShowEdit(true);
+                    setEditData(team);
+                  }}
+                >
+                  edit
+                </p>
+                {showEdit && (
+                  <TeamsModal setShowEdit={setShowEdit} editData={editData} />
+                )}
+                <p onClick={() => handleRemove(team.id)}>remove</p>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
+      <ReactPaginate
+        previousLabel={"previous"}
+        nextLabel={"next"}
+        breakLabel={"..."}
+        pageCount={Math.ceil(teams && teams.length / itemsPerPage)}
+        marginPagesDisplayed={2}
+        pageRangeDisplayed={5}
+        onPageChange={handlePageClick}
+        pageClassName="page-item"
+        pageLinkClassName="page-link"
+        previousClassName="page-item"
+        previousLinkClassName="page-link"
+        nextClassName="page-item"
+        nextLinkClassName="page-link"
+        breakClassName="page-item"
+        breakLinkClassName="page-link"
+        containerClassName="pagination"
+        activeClassName="active"
+        renderOnZeroPageCount={null}
+      />
     </div>
   );
 }
