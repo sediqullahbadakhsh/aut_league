144c68fc2f52e95f0674b86ccba31b4a8fac5289
sediqbadakhsh <sediq@invasso.com>
2023-03-19 04:39:02 +0430

    check if loading

--- src/components/Teams.js
+++ src/components/Teams.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchTeam, removeTeam } from "../store/thunks/teamThunk";
+import { useThunk } from "../hooks/use-thunk";
 import TeamsModal from "./TeamsModal";
 import ViewTeam from "./ViewTeam";
 import ReactPaginate from "react-paginate";
@@ -10,19 +11,23 @@ import { RiDeleteBin6Line } from "react-icons/ri";
 import Dfooter from "./Dfooter";
 
 function Teams() {
-  const teams = useSelector((state) => state.teams.data.teams);
+  const dispatch = useDispatch();
+
+  const [doFetchTeam, isLoadingTeam, loadingTeamError] = useThunk(fetchTeam);
+  const [doRemoveTeam, isLoadingRemoveTeam, loadingRemoveTeamError] =
+    useThunk(removeTeam);
   const [showEdit, setShowEdit] = useState(false);
   const [show, setShow] = useState(false);
   const [viewTeam, setViewTeam] = useState(false);
   const [editData, setEditData] = useState();
   const [viewData, setViewData] = useState();
-  const dispatch = useDispatch();
   const [currentPage, setCurrentPage] = useState(0);
   const itemsPerPage = 10;
 
+  const teams = useSelector((state) => state.teams.data.teams);
   useEffect(() => {
-    dispatch(fetchTeam());
-  }, [dispatch, fetchTeam]);
+    doFetchTeam();
+  }, [doFetchTeam]);
 
   const handleRemove = (id) => {
     dispatch(removeTeam({ id }));
@@ -37,6 +42,71 @@ function Teams() {
     (currentPage + 1) * itemsPerPage
   );
 
+  let content;
+  if (isLoadingTeam) {
+    content = (
+      <div className="table-row">
+        <div>
+          <p className="table-data">در حال بارگذاری</p>;
+        </div>
+      </div>
+    );
+  } else if (loadingTeamError) {
+    content = (
+      <div className="table-row">
+        <div>
+          <p className="table-data">خطا در بارگذاری</p>;
+        </div>
+      </div>
+    );
+  } else {
+    content = (
+      <div className="table-body">
+        {teamsToDisplay?.map((team) => (
+          <div className="table-row" key={team.id}>
+            <p className="table-data">{team.id}</p>
+            <p className="table-data">{team.name}</p>
+            {team.category === 1 ? (
+              <p className="table-data">شبه سازی</p>
+            ) : (
+              <p className="table-data">فزیکی</p>
+            )}
+            <p className="table-data">{team.phone}</p>
+            <p className="table-data">{team.address}</p>
+            <div className="actions table-data">
+              <span
+                className="view-btn"
+                onClick={() => {
+                  setViewTeam(true);
+                  setViewData(team);
+                }}
+              >
+                <AiFillEye />
+              </span>
+
+              <span
+                className="edit-btn"
+                onClick={() => {
+                  setShowEdit(true);
+                  setEditData(team);
+                }}
+              >
+                <BiEdit />
+              </span>
+
+              <span
+                className="delete-btn"
+                onClick={() => handleRemove(team.id)}
+              >
+                <RiDeleteBin6Line />
+              </span>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  }
+
   return (
     <div className="list-main" dir="rtl">
       <h1 className="header_text">تیم ها</h1>
@@ -44,62 +114,23 @@ function Teams() {
         جدید
       </button>
       {show && <TeamsModal setShow={setShow} />}
+      {viewTeam && <ViewTeam viewData={viewData} setViewTeam={setViewTeam} />}
+      {showEdit && <TeamsModal setShowEdit={setShowEdit} editData={editData} />}
       <div>
         <div className="heading"></div>
         <div className="data"></div>
       </div>
-      <table>
-        <thead>
-          <tr>
-            <th>شناسه</th>
-            <th>نام</th>
-            <th>دسته بندی</th>
-            <th>تلفن</th>
-            <th>آدرس</th>
-            <th>اکشن</th>
-          </tr>
-        </thead>
-        <tbody>
-          {teamsToDisplay?.map((team) => (
-            <tr key={team.id}>
-              <td>{team.id}</td>
-              <td>{team.name}</td>
-              {team.category === 1 ? <td>شبه سازی</td> : <td>فزیکی</td>}
-              <td>{team.phone}</td>
-              <td>{team.address}</td>
-              <td className="actions">
-                <p
-                  className="view-btn"
-                  onClick={() => {
-                    setViewTeam(true);
-                    setViewData(team);
-                  }}
-                >
-                  <AiFillEye />
-                </p>
-                {viewTeam && (
-                  <ViewTeam viewData={viewData} setViewTeam={setViewTeam} />
-                )}
-                <p
-                  className="edit-btn"
-                  onClick={() => {
-                    setShowEdit(true);
-                    setEditData(team);
-                  }}
-                >
-                  <BiEdit />
-                </p>
-                {showEdit && (
-                  <TeamsModal setShowEdit={setShowEdit} editData={editData} />
-                )}
-                <p className="delete-btn" onClick={() => handleRemove(team.id)}>
-                  <RiDeleteBin6Line />
-                </p>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <div className="table">
+        <div className="table-heading-container">
+          <p className="table-heading">شناسه</p>
+          <p className="table-heading">نام</p>
+          <p className="table-heading">دسته بندی</p>
+          <p className="table-heading">تلفن</p>
+          <p className="table-heading">آدرس</p>
+          <p className="table-heading">اکشن</p>
+        </div>
+        {content}
+      </div>
       <ReactPaginate
         previousLabel={"قبلی"}
         nextLabel={"بعدی"}
